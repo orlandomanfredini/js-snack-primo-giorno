e5348c05def1d4ac5bf3e46635adcaa5ce9e5294
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-01 16:29:50 +0100

    ADD: es 2

--- js/app.js
+++ js/app.js
@@ -3,3 +3,11 @@
 let number= 100; //number
 
 console.log(number)
+
+//Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+
+const PI = 3.14; //number
+
+console.log(PI)
+
+//
